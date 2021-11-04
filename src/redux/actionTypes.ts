86fe958fcaaeb97eb26d658740e39a1fec7525f6
KhaledTaymour <khaled.taymour@gmail.com>
2021-11-04 22:03:37 +0200

    feat: Add Action Types for store

---
+++ src/redux/actionTypes.ts
@@ -0,0 +1,32 @@
+import { statusEnum, fillDataEnum } from "enums";
+import weatherHourInterface from "interfaces/weatherHourInterface";
+import cityInterface from "interfaces/cityInterface";
+
+interface statusLoadingActionType {
+  type: statusEnum.LOADING;
+}
+
+interface statusFailActionType {
+  type: statusEnum.FAIL;
+}
+
+interface statusSuccessActionType {
+  type: statusEnum.SUCCESS;
+}
+
+interface fillWeatherActionType {
+  type: fillDataEnum.FILL_TEMP;
+  payload: weatherHourInterface[];
+}
+
+interface fillCityActionType {
+  type: fillDataEnum.FILL_CITY;
+  payload: cityInterface;
+}
+
+export type actionDispactchTypes =
+  | statusLoadingActionType
+  | statusFailActionType
+  | statusSuccessActionType
+  | fillWeatherActionType
+  | fillCityActionType;
