c9c7f65366faf98135d64f96260df651ae2873db
KhaledTaymour <khaled.taymour@gmail.com>
2021-11-05 07:10:00 +0200

    test: Apply unit test to utils.
    temperatureUnitConverter,
    dateTimeConverter

---
+++ src/utils/dateTimeConverter.test.ts
@@ -0,0 +1,17 @@
+import {
+  prepareTimeObject,
+  preparedTimeObjectInterface,
+} from "./dateTimeConverter";
+
+describe("dateTimeConverter", () => {
+  test("prepareTimeObject to return object of hours, dayName, date, and month", () => {
+    const preparedTimeObject: preparedTimeObjectInterface = {
+      hours: 14,
+      dayName: "Thursday",
+      date: 16,
+      month: "February",
+    };
+
+    expect(prepareTimeObject(1487246400)).toMatchObject(preparedTimeObject);
+  });
+});
---
+++ src/utils/temperatureUnitConverter.test.ts
@@ -0,0 +1,33 @@
+import {
+  convertTemperatureFromKelvinToCelsius,
+  preparedTemperatureObjectInterface,
+  prepareTemperatureObject,
+} from "./temperatureUnitConverter";
+
+describe("temperatureUnitConverter", () => {
+  test("convertTemperatureFromKelvinToCelsius should convert to celsius correctly", () => {
+    expect(convertTemperatureFromKelvinToCelsius(284)).toEqual("11");
+  });
+
+  test("convertTemperatureFromKelvinToCelsius should not return -0", () => {
+    expect(convertTemperatureFromKelvinToCelsius(273)).not.toEqual("-0");
+    expect(convertTemperatureFromKelvinToCelsius(273)).toEqual("0");
+  });
+
+  test("prepareTemperatureObject should return object of temp, tempMax, and tempMin", () => {
+    const input = {
+      temp: 280,
+      temp_max: 284,
+      temp_min: 274,
+    };
+
+    const preparedTemperatureObject: preparedTemperatureObjectInterface = {
+      temp: "7",
+      tempMax: "11",
+      tempMin: "1",
+    };
+    expect(prepareTemperatureObject(input)).toMatchObject(
+      preparedTemperatureObject
+    );
+  });
+});
