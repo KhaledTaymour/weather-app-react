8b2c23a739665d9ff5e5ee4cda35dd9ca1fa7377
KhaledTaymour <khaled.taymour@gmail.com>
2021-11-08 00:54:00 +0200

    feat: create enums

---
+++ src/enums.ts
@@ -0,0 +1,25 @@
+export enum statusEnum {
+  LOADING = "LOADING",
+  FAIL = "FAIL",
+  SUCCESS = "SUCCESS",
+}
+
+export enum fillDataEnum {
+  FILL_TEMP = "FILL_TEMP",
+  FILL_CITY = "FILL_CITY",
+  CHANGE_SELECTED_CARD = "CHANGE_SELECTED_CARD",
+}
+
+export enum acceptableKeyCodesEnum {
+  SPACE = "Space",
+  ARROWUP = "ArrowUp",
+  ARROWDOWN = "ArrowDown",
+  ARROWLEFT = "ArrowLeft",
+  ARROWRIGHT = "ArrowRight",
+}
+
+export enum weatherCondition {
+  CLEAR = "Clear",
+  CLOUDS = "Clouds",
+  RAIN = "Rain",
+}
